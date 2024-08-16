a26950511641f6d4f6637b436c8946a8e1170d6a
nadineabuodeh <202108847@bethlehem.edu>
2024-08-16 13:29:24 +0300

    design splash and login screen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SplashScreenComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '' }
+];
