37ed0abc5cf2087cfa21592da6fec4c3bd051144
nadineabuodeh <202108847@bethlehem.edu>
2024-08-19 15:08:29 +0300

    stucture of be

--- backend/src/main/java/project/backend/models/Insurance.java
+++ backend/src/main/java/project/backend/models/Insurance.java
@@ -1,26 +1,7 @@
 package project.backend.models;
 
-import java.util.Set;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 
 @Entity
 public class Insurance {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Enumerated(EnumType.STRING)
-    private InsuranceType insuranceType;
-
-    private String description;
-
-    @OneToMany(mappedBy = "insurance")
-    private Set<Policy> policies;
 }
--- backend/src/main/java/project/backend/models/InsuranceType.java
+++ backend/src/main/java/project/backend/models/InsuranceType.java
@@ -1,5 +1,4 @@
 package project.backend.models;
 
 public enum InsuranceType {
-    HEALTH, VEHICLE, HOME, LIFE
 }
