331059dd83910a2e5672b3bdbfffc86395ba4964
nadineabuodeh <202108847@bethlehem.edu>
2024-09-15 12:23:26 +0300

    User Settings Page backend

---
+++ backend/src/main/java/project/backend/SecurityConfiguration/payload/request/UpdateAdminRequest.java
@@ -0,0 +1,68 @@
+package project.backend.SecurityConfiguration.payload.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import project.backend.SecurityConfiguration.models.CurrencyType;
+
+public class UpdateAdminRequest {
+    
+    private CurrencyType currency;
+    
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+    
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 20)
+    private String phoneNumber;
+
+    public CurrencyType getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(CurrencyType currency) {
+        this.currency = currency;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
